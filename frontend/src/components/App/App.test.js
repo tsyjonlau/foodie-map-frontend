5f45ff5c897587bd3c6d043bf7b6204b885ffbce
Tsy-Jon Leslie Lau <tsyjonlau@gmail.com>
2023-10-05 22:22:44 -0400

    fix(App): test

--- frontend/src/components/App/App.test.js
+++ frontend/src/components/App/App.test.js
@@ -3,24 +3,11 @@ import { act, render, screen } from '@testing-library/react';
 
 import App from './App';
 
-beforeEach(() => {
-  fetchMock.mockIf('http://localhost:8000', req => {
-      if (req.url.endsWith('/api/sample')) {
-        return [];
-      } else {
-        return {
-          status: 404,
-          body: 'Not Found'
-        }
-      }
-  })
-});
-
 test('renders learn react link', async () => {
   act(() => {
     render(<App />);
   });
 
-  const linkElement = await screen.findByText(/learn react/i);
+  const linkElement = await screen.findByText(/Foodie Map/i);
   expect(linkElement).toBeInTheDocument();
 });
